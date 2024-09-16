f7202deb08e0f18a015cea760e188311509fcf52
Chris Tse <147206326+isaiahrashadfan@users.noreply.github.com>
2024-09-16 14:24:49 -0700

    Fixed header text color for light mode

--- src/app/computer-science/page.tsx
+++ src/app/computer-science/page.tsx
@@ -9,7 +9,7 @@ export default function ComputerScience() {
 
       
       <div className="flex min-h-screen flex-col items-center justify-center "> 
-        <h1 className="text-4xl text-center md:text-7xl font-Arimo font-bold z-20 pointer-events-none select-none">
+        <h1 className="text-4xl text-center text-black md:text-7xl font-Arimo font-bold z-20 pointer-events-none select-none">
             COMPUTER SCIENCE
         </h1>
         <video playsInline src="/videos/computer science.mp4" autoPlay loop muted preload='auto' width='1280' height='720' className="object-cover absolute w-screen h-screen justify-center z-0 pointer-events-none"></video>
--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -6,7 +6,7 @@ export default function Music() {
 
       
       <div className="flex min-h-screen flex-col items-center justify-center "> 
-        <h1 className="text-4xl text-center md:text-7xl font-Arimo font-bold z-20 pointer-events-none select-none">
+        <h1 className="text-4xl text-center text-black md:text-7xl font-Arimo font-bold z-20 pointer-events-none select-none">
             MUSIC
         </h1>
         <video playsInline src="/videos/music.mp4" autoPlay loop muted preload='auto' width='1280' height='720' className="object-cover absolute w-screen h-screen justify-center z-0 pointer-events-none"></video>
